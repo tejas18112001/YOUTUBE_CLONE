1587d64cfbbb6b9e26387aca165e5cebd17e5187
Tejas <tejas.22010583@viit.ac.in>
2023-08-19 15:12:16 +0530

    commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, CircularProgress, Stack } from '@mui/material';
+
+const Loader = () =>  (
+ <div>
+    Loader
+ </div>
+);
+
+export default Loader;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,82 @@
+
+import * as React from 'react';
+
+import { styled} from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor:'#d8dee3',
+  '&:hover': {
+    backgroundColor: '#FFFFFF',
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  color : 'red' ,
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+const  SearchBar = ()=> (
+    <Box sx={{ flexGrow: 1 }}>
+       <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            
+            aria-label="open drawer"
+            sx={{mr: 140 }}
+          >
+          
+          </IconButton>
+         
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+    </Box>
+  );
+
+export default SearchBar;
